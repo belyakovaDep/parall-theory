1caee92c1183288db12b1d46c05d7c4d6e83b983
d.belyakova1 <d.belyakova1@g.nsu.ru>
2024-03-18 18:15:50 +0700

    changes 1

--- lab2/task3/varPar/task.cpp
+++ lab2/task3/varPar/task.cpp
@@ -1,132 +1,130 @@
 #include <iostream>
 #include <omp.h>
 #include <cstring>
+#include <memory>
 
 double cpuSecond()
 {
-	struct timespec ts;
-	timespec_get(&ts, TIME_UTC);
-	return ((double)ts.tv_sec + (double)ts.tv_nsec * 1.e-9);
+    struct timespec ts;
+    timespec_get(&ts, TIME_UTC);
+    return ((double)ts.tv_sec + (double)ts.tv_nsec * 1.e-9);
 }
 
-void integration(const double* matrix, const double* b, const double* x, double** res, int n)
+void integration(const double* matrix, const double* b, const double* x, double* res, int n)
 {
 #pragma omp parallel
 {
     int nthreads = omp_get_num_threads();
-	int threadid = omp_get_thread_num();
-	int items_per_thread = n / nthreads;
-	int lb = threadid * items_per_thread;
-	int ub = (threadid == nthreads - 1) ? (n - 1) : (lb + items_per_thread - 1);
+    int threadid = omp_get_thread_num();
+    int items_per_thread = n / nthreads;
+    int lb = threadid * items_per_thread;
+    int ub = (threadid == nthreads - 1) ? (n - 1) : (lb + items_per_thread - 1);
 
     for(int i = lb; i <= ub; i++)
     {
-        (*res)[i] = 0.0;
+        res[i] = 0.0;
 
-			for (int j = 0; j < n; j++)
-			{
-				(*res)[i] += matrix[i * n + j] * x[j];
-			}
-
+        for (int j = 0; j < n; j++)
+        {
+            res[i] += matrix[i * n + j] * x[j];
+        }
     }
 
     for (int i = lb; i <= ub; i++)
-	{
-		(*res)[i] = (*res)[i] - b[i];
-	}
+    {
+        res[i] = res[i] - b[i];
+    }
 
     for (int i = lb; i <= ub; i++)
-	{
-		(*res)[i] = (*res)[i] * (0.0001);
-	}
+    {
+        res[i] = res[i] * (0.0001);
+    }
 
-	for (int i = lb; i <= ub; i++)
-	{
-		(*res)[i] = x[i] - (*res)[i];
-	}
+    for (int i = lb; i <= ub; i++)
+    {
+        res[i] = x[i] - res[i];
+    }
 }
 }
 
-void cycleChecking(const double* matrix, const double* b, double** x, double** res, int n)
+void cycleChecking(const double* matrix, const double* b, double* x, double* res, int n)
 {
-	double* check = (double*)malloc(sizeof(double) * n);
-	double misUp = 0.0, misDown = 0.0, mis;
+    std::unique_ptr<double[]> check(new double[n]);
+    double misUp = 0.0, misDown = 0.0, mis = 1;
 
-	while(1)
-	{
-		integration(matrix, b, *x, res, n);
+    while(mis > 0.00001)
+    {
+        integration(matrix, b, x, res, n);
 
-		misUp = 0.0; misDown = 0.0;
+        misUp = 0.0;
+        misDown = 0.0;
 
-		#pragma omp parallel
-		{
-			int nthreads = omp_get_num_threads();
-			int threadid = omp_get_thread_num();
-			int items_per_thread = n / nthreads;
-			int lb = threadid * items_per_thread;
-			int ub = (threadid == nthreads - 1) ? (n - 1) : (lb + items_per_thread - 1);
+#pragma omp parallel
+        {
+            int nthreads = omp_get_num_threads();
+            int threadid = omp_get_thread_num();
+            int items_per_thread = n / nthreads;
+            int lb = threadid * items_per_thread;
+            int ub = (threadid == nthreads - 1) ? (n - 1) : (lb + items_per_thread - 1);
 
-			double localsum = 0.0, localsumB = 0.0;
+            double localsum = 0.0, localsumB = 0.0;
 
-			for(int i = lb; i <= ub; i++)
-    		{
-        		check[i] = 0.0;
+            for(int i = lb; i <= ub; i++)
+            {
+                check[i] = 0.0;
 
-				for (int j = 0; j < n; j++)
-				{
-					check[i] += matrix[i * n + j] * (*x)[j];
-				}
-    		}
+                for (int j = 0; j < n; j++)
+                {
+                    check[i] += matrix[i * n + j] * x[j];
+                }
+            }
 
-			for (int i = lb; i <= ub; i++)
-			{
-				check[i] = check[i] - b[i];
-				localsum += check[i] * check[i];
-				localsumB += b[i] * b[i];
-			}
+            for (int i = lb; i <= ub; i++)
+            {
+                check[i] = check[i] - b[i];
+                localsum += check[i] * check[i];
+                localsumB += b[i] * b[i];
+            }
 
-			#pragma omp atomic
-				misUp += localsum;
-				misDown += localsumB;
-		}
+#pragma omp atomic
+            misUp += localsum;
+#pragma omp atomic
+            misDown += localsumB;
+        }
 
-		mis = misUp/misDown;
-		if(mis <= 0.00001) break;
+        mis = misUp/misDown;
 
-		memcpy(*x, *res, sizeof(double) * n);
-	}
+        std::memcpy(x, res, sizeof(double) * n);
+    }
 }
 
 int main()
 {
-	int n = 17000;
-	double* matrix, * vector, * results, *changed_one;
+    int n = 17000;
+    std::unique_ptr<double[]> matrix(new double[n * n]);
+    std::unique_ptr<double[]> vector(new double[n]);
+    std::unique_ptr<double[]> results(new double[n]);
+    std::unique_ptr<double[]> changed_one(new double[n]);
 
-	matrix = (double*)malloc(sizeof(double) * n * n);
-	vector = (double*)malloc(sizeof(double) * n);
-	results = (double*)malloc(sizeof(double) * n);
-	changed_one = (double*)malloc(sizeof(double) * n);
+    for (int i = 0; i < n; i++)
+    {
+        vector[i] = n + 1;
+        results[i] = 0;
 
-	for (int i = 0; i < n; i++)
-	{
-		vector[i] = n + 1;
-		results[i] = 0;
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i * n + j] = (i == j) ? 2.0 : 1.0;
+        }
+    }
 
-		for (int j = 0; j < n; j++)
-		{
-			matrix[i * n + j] = (i == j) ? 2.0 : 1.0;
-		}
-	}
+    omp_set_num_threads(80);
 
-	omp_set_num_threads(80);
+    double t = cpuSecond();
 
-	double t = cpuSecond();
+    cycleChecking(matrix.get(), vector.get(), results.get(), changed_one.get(), n);
 
-    cycleChecking(matrix, vector, &results, &changed_one, n);
+    t = cpuSecond() - t;
 
-	t = cpuSecond() - t;
-
-	std::cout << t << std::endl;
-	std::cout<< changed_one[0] << " " << changed_one[1] << std::endl;
+    std::cout << t << std::endl;
     return 0;
 }
--- lab2/task3/withFor/task.cpp
+++ lab2/task3/withFor/task.cpp
@@ -1,109 +1,107 @@
 #include <iostream>
-#include <math.h>
+#include <cmath>
 #include <omp.h>
 #include <string>
 #include <cstring>
+#include <memory>
+
 void omp_set_num_threads(int num_threads);
 
 double cpuSecond()
 {
-	struct timespec ts;
-	timespec_get(&ts, TIME_UTC);
-	return ((double)ts.tv_sec + (double)ts.tv_nsec * 1.e-9);
+    struct timespec ts;
+    timespec_get(&ts, TIME_UTC);
+    return ((double)ts.tv_sec + (double)ts.tv_nsec * 1.e-9);
 }
 
-void matrix_vector_product_omp(double* a, double* b, double** c, int n)
+void matrix_vector_product_omp(double* a, double* b, double* c, int n)
 {
-#pragma omp parallel for schedule (dynamic, 50)
-	for (int i = 0; i <= n; i++)
-	{
-		(*c)[i] = 0.0;
-		for (int j = 0; j < n; j++)
-		{
-			(*c)[i] += a[i * n + j] * b[j];
-		}
-	}
+#pragma omp parallel for schedule(dynamic, 50)
+    for (int i = 0; i < n; i++)
+    {
+        c[i] = 0.0;
+        for (int j = 0; j < n; j++)
+        {
+            c[i] += a[i * n + j] * b[j];
+        }
+    }
 }
 
-void substraction(double* a, double* b, double** c, int n)
+void substraction(double* a, double* b, double* c, int n)
 {
-#pragma omp parallel for schedule (dynamic, 50)
-	for (int i = 0; i < n; i++)
-	{
-		(*c)[i] = a[i] - b[i];
-	}
+#pragma omp parallel for schedule(dynamic, 50)
+    for (int i = 0; i < n; i++)
+    {
+        c[i] = a[i] - b[i];
+    }
 }
 
-void mult(double* a, double t, double** c, int n)
+void mult(double* a, double t, double* c, int n)
 {
-#pragma omp parallel for schedule (dynamic, 50)
-	for (int i = 0; i < n; i++)
-	{
-		(*c)[i] = a[i] * t;
-	}
+#pragma omp parallel for schedule(dynamic, 50)
+    for (int i = 0; i < n; i++)
+    {
+        c[i] = a[i] * t;
+    }
 }
 
 void iteration_alg(double* matrix, double* x, double* b, double* newX, int n)
 {
-	matrix_vector_product_omp(matrix, x, &newX, n);
-	substraction(newX, b, &newX, n);
-	mult(newX, 0.0001, &newX, n);
-	substraction(x, newX, &newX, n);
+    matrix_vector_product_omp(matrix, x, newX, n);
+    substraction(newX, b, newX, n);
+    mult(newX, 0.0001, newX, n);
+    substraction(x, newX, newX, n);
 }
 
 bool check_alg(double* matrix, double* x, double* b, double* checking, int n)
 {
-	double check = 0, bcheck = 0, res;
-	matrix_vector_product_omp(matrix, x, &checking, n);
-	substraction(checking, b, &checking, n);
+    double check = 0, bcheck = 0, res;
+    matrix_vector_product_omp(matrix, x, checking, n);
+    substraction(checking, b, checking, n);
 
-	for (int i = 0; i < n; i++)
-	{
-		check += checking[i] * checking[i];
-		bcheck += b[i] * b[i];
-	}
-	res = check / bcheck;
+    for (int i = 0; i < n; i++)
+    {
+        check += checking[i] * checking[i];
+        bcheck += b[i] * b[i];
+    }
+    res = check / bcheck;
 
-	if (res <= 0.00001) return true;
-	return false;
+    if (res <= 0.00001) return true;
+    return false;
 }
 
 int main()
 {
-	double t = cpuSecond();
+    double t = cpuSecond();
 
-	int n = 17000;
-	double* matrix, * vector, * results, *changed_one;
+    int n = 17000;
+    std::unique_ptr<double[]> matrix(new double[n * n]);
+    std::unique_ptr<double[]> vector(new double[n]);
+    std::unique_ptr<double[]> results(new double[n]);
+    std::unique_ptr<double[]> changed_one(new double[n]);
 
-	matrix = (double*)malloc(sizeof(double) * n * n);
-	vector = (double*)malloc(sizeof(double) * n);
-	results = (double*)malloc(sizeof(double) * n);
-	changed_one = (double*)malloc(sizeof(double) * n);
+    for (int i = 0; i < n; i++)
+    {
+        vector[i] = n + 1;
+        results[i] = 0;
 
-	for (int i = 0; i < n; i++)
-	{
-		vector[i] = n + 1;
-		results[i] = 0;
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i * n + j] = (i == j) ? 2.0 : 1.0;
+        }
+    }
 
-		for (int j = 0; j < n; j++)
-		{
-			matrix[i * n + j] = (i == j) ? 2.0 : 1.0;
-		}
-	}
+    omp_set_num_threads(20);
 
-	omp_set_num_threads(20);
+    while(!check_alg(matrix.get(), results.get(), vector.get(), changed_one.get(), n))
+    {
+        iteration_alg(matrix.get(), results.get(), vector.get(), changed_one.get(), n);
+        std::memcpy(results.get(), changed_one.get(), sizeof(double) * n);
+    }
 
-	while(1)
-	{
-		iteration_alg(matrix, results, vector, changed_one, n);
-		memcpy(results, changed_one, sizeof(double) * n);
+    t = cpuSecond() - t;
 
-		if (check_alg(matrix, results, vector, changed_one, n)) break;
-	}
+    std::cout << t;
 
-	t = cpuSecond() - t;
-
-	std::cout << t;
-
-	return 0;
+    return 0;
 }
